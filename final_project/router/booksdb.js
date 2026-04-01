bfd301494b7899c0bd22c53faf87e1883c46c54b
tifffos2038-ctrl <tifffos2038@gmail.com>
2026-04-01 16:55:08 -0500

    Refactor book entries to remove 'copies' property
    Removed 'copies' property from book entries.

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,58 +1,14 @@
-const express = require('express');
-const router = express.Router();
-
 let books = {
-      1: {
-            "author": "Chinua Achebe",
-            "title": "Things Fall Apart",
-            "copies":10, 
-            "reviews": {} 
-      },
-      2: {
-            "author": "Hans Christian Andersen",
-            "title": "Fairy tales", 
-            "copies":10, 
-            "reviews": {0: "bad", 1: "good"} },
-      3: {
-            "author": "Dante Alighieri",
-            "title": "The Divine Comedy", 
-            "copies":10, 
-            "reviews": {} },
-      4: {
-            "author": "Unknown",
-            "title": "The Epic Of Gilgamesh",
-            "copies":10, 
-            "reviews": {} },
-      5: {
-            "author": "Unknown",
-            "title": "The Book Of Job",
-            "copies":10, 
-            "reviews": {} },
-      6: {
-            "author": "Unknown",
-            "title": "One Thousand and One Nights",
-            "copies":10, 
-            "reviews": {} },
-      7: {
-            "author": "Unknown",
-            "title": "Nj\u00e1l's Saga",
-            "copies":10, 
-            "reviews": {} },
-      8: {
-            "author": "Jane Austen",
-            "title": "Pride and Prejudice",
-            "copies":10, 
-            "reviews": {} },
-      9: {
-            "author": "Honor\u00e9 de Balzac",
-            "title": "Le P\u00e8re Goriot",
-            "copies":10, 
-            "reviews": {} },
-      10: {"author": "Samuel Beckett",
-            "title": "Molloy, Malone Dies, The Unnamable, the trilogy",
-            "copies":10, 
-            "reviews": {} 
-      }
+      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
+      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
+      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
+      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
+      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
+      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
+      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
+      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
+      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
+      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
 }
 
 module.exports=books;
